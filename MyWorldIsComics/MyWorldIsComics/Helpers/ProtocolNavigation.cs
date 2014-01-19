3a5f3b04bb631e8d15af91b2c4c8fa1896e8f424
Tom Finlayson <tomfin46@gmail.com>
2014-01-19 21:38:25 +0000

    Protocol Nav Finalisation
    Finalising protocol navigation
    Enabling nav to and from story arc and object pages

--- MyWorldIsComics/MyWorldIsComics/Helpers/ProtocolNavigation.cs
+++ MyWorldIsComics/MyWorldIsComics/Helpers/ProtocolNavigation.cs
@@ -32,31 +32,7 @@ namespace MyWorldIsComics.Helpers
                     var characterPage = rootFrame.Content as CharacterPage;
                     if (characterPage != null) NavigateToProtocolPage(characterPage.Frame);
                     break;
-                case "IssuePage":
-                    var issuePage = rootFrame.Content as IssuePage;
-                    if (issuePage != null) NavigateToProtocolPage(issuePage.Frame);
-                    break;
-                case "TeamPage":
-                    var teamPage = rootFrame.Content as TeamPage;
-                    if (teamPage != null) NavigateToProtocolPage(teamPage.Frame);
-                    break;
-                case "DescriptionSectionPage":
-                    var descriptionSection = rootFrame.Content as DescriptionSectionPage;
-                    if (descriptionSection != null) NavigateToProtocolPage(descriptionSection.Frame);
-                    break;
-                case "VolumePage":
-                    var volumePage = rootFrame.Content as VolumePage;
-                    if (volumePage != null) NavigateToProtocolPage(volumePage.Frame);
-                    break;
-                case "LocationPage":
-                    var locationPage = rootFrame.Content as LocationPage;
-                    if (locationPage != null) NavigateToProtocolPage(locationPage.Frame);
-                    break;
-                case "PublisherPage":
-                    var publisherPage = rootFrame.Content as PublisherPage;
-                    if (publisherPage != null) NavigateToProtocolPage(publisherPage.Frame);
-                    break;
-                case "ConceptPage":
+                 case "ConceptPage":
                     var conceptPage = rootFrame.Content as ConceptPage;
                     if (conceptPage != null) NavigateToProtocolPage(conceptPage.Frame);
                     break;
@@ -64,9 +40,41 @@ namespace MyWorldIsComics.Helpers
                     var creatorPage = rootFrame.Content as CreatorPage;
                     if (creatorPage != null) NavigateToProtocolPage(creatorPage.Frame);
                     break;
+                case "IssuePage":
+                    var issuePage = rootFrame.Content as IssuePage;
+                    if (issuePage != null) NavigateToProtocolPage(issuePage.Frame);
+                    break;
+                case "LocationPage":
+                    var locationPage = rootFrame.Content as LocationPage;
+                    if (locationPage != null) NavigateToProtocolPage(locationPage.Frame);
+                    break;
                 case "MoviePage":
                     var moviePage = rootFrame.Content as MoviePage;
                     if (moviePage != null) NavigateToProtocolPage(moviePage.Frame);
+                    break;
+                case "ObjectPage":
+                    var objectPage = rootFrame.Content as ObjectPage;
+                    if (objectPage != null) NavigateToProtocolPage(objectPage.Frame);
+                    break;
+                case "PublisherPage":
+                    var publisherPage = rootFrame.Content as PublisherPage;
+                    if (publisherPage != null) NavigateToProtocolPage(publisherPage.Frame);
+                    break;
+                case "StoryArcPage":
+                    var storyArcPage = rootFrame.Content as StoryArcPage;
+                    if (storyArcPage != null) NavigateToProtocolPage(storyArcPage.Frame);
+                    break;
+                case "TeamPage":
+                    var teamPage = rootFrame.Content as TeamPage;
+                    if (teamPage != null) NavigateToProtocolPage(teamPage.Frame);
+                    break;
+                case "VolumePage":
+                    var volumePage = rootFrame.Content as VolumePage;
+                    if (volumePage != null) NavigateToProtocolPage(volumePage.Frame);
+                    break;
+                case "DescriptionSectionPage":
+                    var descriptionSection = rootFrame.Content as DescriptionSectionPage;
+                    if (descriptionSection != null) NavigateToProtocolPage(descriptionSection.Frame);
                     break;
             }
         }
@@ -97,10 +105,10 @@ namespace MyWorldIsComics.Helpers
                     pageFrame.Navigate(typeof(ConceptPage), _protocolEventArgs.Uri.Segments[2]);
                     break;
                 case "4055":
-                    // TODO pageFrame.Navigate(typeof(ObjectPage), _protocolEventArgs.Uri.Segments[2]);
+                    pageFrame.Navigate(typeof(ObjectPage), _protocolEventArgs.Uri.Segments[2]);
                     break;
                 case "4045":
-                    // TODO pageFrame.Navigate(typeof(StoryArcPage), _protocolEventArgs.Uri.Segments[2]);
+                     pageFrame.Navigate(typeof(StoryArcPage), _protocolEventArgs.Uri.Segments[2]);
                     break;
                 case "4050":
                      pageFrame.Navigate(typeof(VolumePage), _protocolEventArgs.Uri.Segments[2]);
